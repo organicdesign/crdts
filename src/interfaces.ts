c959ddcbd2ad870b69df660b3a2c2e2feb8dd69d
saul <saul@organicdesign.co.nz>
2022-12-01 13:46:18 +1300

    Add interfaces for generic data types.

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -5,3 +5,22 @@ export interface CRDT {
 }
 
 export type deserialize = <T extends CRDT=CRDT>(data: Uint8Array) => T;
+
+export interface GCounter {
+  increment (quantity: number): void
+}
+
+export interface PNCounter extends GCounter {
+  decrement (quantity: number): void
+}
+
+export interface Register<T=unknown> {
+  get (): T
+  set (value: T): void
+}
+
+export type GMap<T extends CRDT=CRDT> = Omit<Map<string, T>, "clear" | "delete" | typeof Symbol.toStringTag>;
+export type PNMap<T=unknown> = Omit<Map<string, T>, typeof Symbol.toStringTag>;
+
+export type GSet<T=unknown> = Omit<Set<T>, "clear" | "delete" | typeof Symbol.toStringTag>;
+export type PNSet<T=unknown> = Omit<Set<T>, typeof Symbol.toStringTag>;
