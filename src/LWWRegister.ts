e2e169219a81373caab94e297b0906cd9f89e1f2
saul <saul@organicdesign.co.nz>
2022-12-02 15:26:49 +1300

    Add LWW register type.

---
+++ src/LWWRegister.ts
@@ -0,0 +1,45 @@
+import * as cborg from "cborg";
+import { CRDT } from "./CRDT.js";
+import type { CRDT as ICRDT, Register } from "./interfaces.js";
+
+export class LWWRegister<T> extends CRDT implements ICRDT, Register<T> {
+  private data: T;
+  private timestamp: string = "";
+
+  get(): T {
+    return this.data;
+  }
+
+  set(value: T): void {
+    this.data = value;
+    this.timestamp = this.generateTimestamp();
+  }
+
+  sync(data?: Uint8Array): Uint8Array | undefined {
+    if (data == null) {
+      return this.serialize();
+    }
+
+    const { value, timestamp }: { value: T, timestamp: string } = cborg.decode(data);
+
+    if (timestamp > this.timestamp) {
+      this.data = value;
+      this.timestamp = timestamp;
+    }
+  }
+
+  toValue(): T {
+    return this.data;
+  }
+
+  serialize(): Uint8Array {
+    return cborg.encode({
+      value: this.data,
+      timestamp: this.timestamp
+    });
+  }
+
+  onBroadcast?(data: Uint8Array): void {
+    throw new Error("Method not implemented.");
+  }
+}
