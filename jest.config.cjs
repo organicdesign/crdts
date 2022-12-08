0e8467570b6de79a5d2ae3b53f731c42b78f2cff
saul <saul@organicdesign.co.nz>
2022-12-09 11:07:01 +1300

    Improve jest config.

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,12 +3,14 @@ module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
 	maxWorkers: 1,
+	extensionsToTreatAsEsm: [".ts"],
 	moduleNameMapper: {
 		"^(\\.{1,2}/.*)\\.js$": "$1"
 	},
 	transform: {
 		// '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
 		// '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+		// "^.+\\.tsx?$"
 		"^.+\\.tsx?$": [
 			"ts-jest",
 			{
