4109d8890f8c1d5055c8cb6f7e29d39a6dc2764a
saul <saul@organicdesign.co.nz>
2022-12-09 11:44:49 +1300

    Export all CRDTs.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export { CRDTMap } from "./CRDTMap.js";
+export { createGCounter, deserializeGCounter, GCounter } from "./GCounter.js";
+export { createPNCounter, PNCounter } from "./PNCounter.js";
+export { createGSet, GSet } from "./GSet.js";
+export { createLWWRegister, LWWRegister } from "./LWWRegister.js";
+export { createLWWMap, LWWMap } from "./LWWMap.js";
+export { createCRDTMap, CRDTMap } from "./CRDTMap.js";
