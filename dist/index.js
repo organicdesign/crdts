5f36127a2e135bf8f22e7b761ece5dcdd771e999
saul <saul@organicdesign.co.nz>
2023-01-11 12:48:51 +1300

    Build.

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 export { createGCounter, GCounter } from "./GCounter.js";
 export { createPNCounter, PNCounter } from "./PNCounter.js";
 export { createGSet, GSet } from "./GSet.js";
+export { createMVRegister, MVRegister } from "./MVRegister.js";
 export { createLWWRegister, LWWRegister } from "./LWWRegister.js";
 export { createLWWMap, LWWMap } from "./LWWMap.js";
 export { createCRDTMap, CRDTMap } from "./CRDTMap.js";
