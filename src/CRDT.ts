0515d8e7a68cea4bb7f86a6c1651334c94b8c247
saul <saul@organicdesign.co.nz>
2023-01-30 15:29:19 +1300

    Rework CRDT base class to new system.

--- src/CRDT.ts
+++ src/CRDT.ts
@@ -1,15 +1,22 @@
-import type { CRDTConfig } from "@organicdesign/crdt-interfaces";
+import type {
+	CRDTConfig,
+	CRDTSynchronizer,
+	CRDTSerializer,
+	CRDTBroadcaster,
+	CRDT as ICRDT,
+	SerializableCRDT,
+	SynchronizableCRDT,
+	BroadcastableCRDT
+} from "@organicdesign/crdt-interfaces";
 
-export class CRDT {
+export class CRDT implements Omit<ICRDT & SerializableCRDT & SynchronizableCRDT & BroadcastableCRDT, "toValue"> {
 	protected readonly config: CRDTConfig;
-	private readonly broadcasters: ((data: Uint8Array) => void)[] = [];
+	protected readonly synchronizers: CRDTSynchronizer[] = [];
+	protected readonly serializers: CRDTSerializer[] = [];
+	protected readonly broadcasters: CRDTBroadcaster[] = [];
 
 	constructor (config: CRDTConfig) {
 		this.config = config;
-	}
-
-	addBroadcaster (broadcaster: (data: Uint8Array) => void) {
-		this.broadcasters.push(broadcaster);
 	}
 
 	get id () : Uint8Array {
@@ -20,9 +27,27 @@ export class CRDT {
 		return this.config.generateTimestamp ?? Date.now;
 	}
 
-	protected broadcast (data: Uint8Array) {
-		for (const broadcaster of this.broadcasters) {
-			broadcaster(data);
-		}
+	getSynchronizer (protocol: string): CRDTSynchronizer | undefined {
+		return this.synchronizers.find(s => s.protocol === protocol);
+	}
+
+	getSynchronizerProtocols (): Iterable<string> {
+		return this.synchronizers.map(s => s.protocol);
+	}
+
+	getSerializer (protocol: string): CRDTSerializer | undefined {
+		return this.serializers.find(s => s.protocol === protocol);
+	}
+
+	getSerializeProtocols (): Iterable<string> {
+		return this.serializers.map(s => s.protocol);
+	}
+
+	getBroadcaster (protocol: string): CRDTBroadcaster | undefined {
+		return this.broadcasters.find(s => s.protocol === protocol);
+	}
+
+	getBroadcastProtocols (): Iterable<string> {
+		return this.broadcasters.map(s => s.protocol);
 	}
 }
