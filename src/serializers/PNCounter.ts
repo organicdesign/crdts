b0356ea90ff9bc48316773943db7faf39b18b838
saul <saul@organicdesign.co.nz>
2023-02-02 11:37:29 +1300

    Create PNCounter serializer.

---
+++ src/serializers/PNCounter.ts
@@ -0,0 +1,46 @@
+import type { CRDTSerializer, SerializableCRDT } from "@organicdesign/crdt-interfaces";
+import * as cborg from "cborg";
+
+export interface PNCounterSerializerComponents {
+	getPCounter (): SerializableCRDT
+	getNCounter (): SerializableCRDT
+}
+
+export interface PNCounterSyncOpts {
+	protocol: string
+	subProtocol: string
+}
+
+export class PNCounterSerializer implements CRDTSerializer {
+	private readonly components: PNCounterSerializerComponents;
+	private readonly options: PNCounterSyncOpts;
+
+	constructor(components: PNCounterSerializerComponents, options: Partial<PNCounterSyncOpts> = {}) {
+		this.options = {
+			protocol: options.protocol ?? "/pn-counter/cbor/0.1.0",
+			subProtocol: options.subProtocol ?? "/g-counter/cbor/0.1.0"
+		}
+
+		this.components = components;
+	}
+
+	get protocol () {
+		return this.options.protocol;
+	}
+
+	serialize (): Uint8Array {
+		return cborg.encode({
+			pData: this.components.getPCounter().getSerializer(this.options.subProtocol)?.serialize(),
+			nData: this.components.getNCounter().getSerializer(this.options.subProtocol)?.serialize()
+		});
+	}
+
+	deserialize (data: Uint8Array) {
+		const { pData, nData } = cborg.decode(data) as { pData: Uint8Array, nData: Uint8Array };
+
+		this.components.getPCounter().getSerializer(this.options.subProtocol)?.deserialize(pData);
+		this.components.getNCounter().getSerializer(this.options.subProtocol)?.deserialize(nData);
+	}
+}
+
+export const createPNCounterSerializer = (options?: Partial<PNCounterSyncOpts>) => (components: PNCounterSerializerComponents) => new PNCounterSerializer(components, options);
