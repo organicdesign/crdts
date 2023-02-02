d8b75fba500311fc8ff34ce08e4c139cb7fbf7b3
saul <saul@organicdesign.co.nz>
2023-02-02 14:44:01 +1300

    Add GSet serializer.

---
+++ src/serializers/GSet.ts
@@ -0,0 +1,35 @@
+import type { CRDTSerializer } from "@organicdesign/crdt-interfaces";
+import * as cborg from "cborg";
+
+export interface GSetSerializerComponents {
+	get (): Iterable<unknown>
+	add (item: unknown): void
+}
+
+export interface GSetSyncOpts {
+	protocol: string
+}
+
+export class GSetSerializer implements CRDTSerializer {
+	public readonly protocol: string;
+	private readonly components: GSetSerializerComponents;
+
+	constructor(components: GSetSerializerComponents, options: Partial<GSetSyncOpts> = {}) {
+		this.protocol = options.protocol ?? "/g-set/cbor/0.1.0";
+		this.components = components;
+	}
+
+	serialize (): Uint8Array {
+		return cborg.encode([...this.components.get()]);
+	}
+
+	deserialize (data: Uint8Array) {
+		const items = cborg.decode(data) as unknown[];
+
+		for (const item of items) {
+			this.components.add(item);
+		}
+	}
+}
+
+export const createGSetSerializer = (options?: Partial<GSetSyncOpts>) => (components: GSetSerializerComponents) => new GSetSerializer(components, options);
